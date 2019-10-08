c115cc55981a86559c1cfe049cb9e168df320066
Theo <tstavrianos@users.noreply.github.com>
2019-10-08 14:18:39 +0100

    Chapter 1 done.

---
+++ Kaleidoscope/Lexer/Token.cs
@@ -0,0 +1,11 @@
+namespace Kaleidoscope.Lexer
+{
+    public static class Token
+    {
+        public const int Eof = -1;
+        public const int Def = -2;
+        public const int Extern = -3;
+        public const int Identifier = -4;
+        public const int Number = -5;
+    }
+}
